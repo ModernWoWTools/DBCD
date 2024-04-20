dec2367dd8d875c49334fd08a7d2d3778de19a66
Luzifix <luzifix19@gmail.com>
2024-04-20 20:24:37 +0200

    Add missing wdc5 section header

--- DBFileReaderLib/Common/DBStructs.cs
+++ DBFileReaderLib/Common/DBStructs.cs
@@ -207,4 +207,20 @@ namespace DBFileReaderLib.Common
         int IEncryptableDatabaseSection.NumRecords => this.NumRecords;
     }
 
+    [StructLayout(LayoutKind.Sequential, Pack = 2)]
+    struct SectionHeaderWDC5 : IEncryptableDatabaseSection
+    {
+        public ulong TactKeyLookup;
+        public int FileOffset;
+        public int NumRecords;
+        public int StringTableSize;
+        public int OffsetRecordsEndOffset; // CatalogDataOffset, absolute value, {uint offset, ushort size}[MaxId - MinId + 1]
+        public int IndexDataSize; // int indexData[IndexDataSize / 4]
+        public int ParentLookupDataSize; // uint NumRecords, uint minId, uint maxId, {uint id, uint index}[NumRecords], questionable usefulness...
+        public int OffsetMapIDCount;
+        public int CopyTableCount;
+
+        ulong IEncryptableDatabaseSection.TactKeyLookup => this.TactKeyLookup;
+        int IEncryptableDatabaseSection.NumRecords => this.NumRecords;
+    }
 }
